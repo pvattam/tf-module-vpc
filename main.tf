bc4c9c7f4bfd009f9675b540897338ea5eb6eecd
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-07 08:22:28 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- main.tf
+++ main.tf
@@ -38,27 +38,6 @@ resource "aws_subnet" "db" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "public"})
-}
-
-resource "aws_route_table" "web" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(var.tags, {Name = "web"})
-}
-
-resource "aws_route_table" "app" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(var.tags, {Name = "app"})
-}
-
-resource "aws_route_table" "db" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(var.tags, {Name = "db"})
-}
-
-resource "aws_route_table_association" "public" {
-  count = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public.*.id[count.index]
-  route_table_id = aws_route_table.public.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -71,10 +50,9 @@ resource "aws_route_table_association" "public" {
   }
 }
 
-resource "aws_route_table_association" "web" {
-  count = length(aws_subnet.web)
-  subnet_id      = aws_subnet.web.*.id[count.index]
-  route_table_id = aws_route_table.web.id
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "web"})
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -85,38 +63,60 @@ resource "aws_route_table_association" "web" {
     cidr_block = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
+}
+
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "app"})
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+}
+
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "db"})
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "web" {
+  count = length(aws_subnet.web)
+  subnet_id      = aws_subnet.web.*.id[count.index]
+  route_table_id = aws_route_table.web.id
 }
 
 resource "aws_route_table_association" "app" {
   count = length(aws_subnet.app)
   subnet_id      = aws_subnet.app.*.id[count.index]
   route_table_id = aws_route_table.app.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
-
-  route {
-    cidr_block = var.default_vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  }
 }
 
 resource "aws_route_table_association" "db" {
   count = length(aws_subnet.db)
   subnet_id      = aws_subnet.db.*.id[count.index]
   route_table_id = aws_route_table.db.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
-
-  route {
-    cidr_block = var.default_vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  }
 }
 
 resource "aws_internet_gateway" "igw" {
