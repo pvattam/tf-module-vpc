ec3b7242bbe675c18a52eff0e4c24f9ffe23f479
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-07 08:11:20 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr_block" {}
+variable "public_subnet" {}
+variable "web_subnet" {}
+variable "app_subnet" {}
+variable "db_subnet" {}
+variable "azs" {}
+variable "tags" {}
+variable "env" {}
+variable "account_id" {}
+variable "default_vpc_id" {}
+variable "default_route_table_id" {}
+variable "default_vpc_cidr" {}
+
+
